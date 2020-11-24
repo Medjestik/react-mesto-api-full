3a246294107eb0d8db257a81e7ff4e68bcaf0f6d
Medjestik <ivan-kostyulin@yandex.ru>
2020-11-24 07:51:54 +0300

    fix: controlller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -18,7 +18,7 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.id)
+  User.find({})
     .then((user) => {
       res
         .status(200)

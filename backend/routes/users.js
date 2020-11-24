9d126ed64323aad3b9fb143b1e385ef9f3820984
Medjestik <ivan-kostyulin@yandex.ru>
2020-11-24 20:22:39 +0300

    fix: gitignore

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 
 const {
-  getUsers, getUserByToken, getUserById, editProfile, editAvatar,
+  getUserByToken, getUserById, editProfile, editAvatar,
 } = require('../controllers/users');
 
-router.get('/', getUsers);
 router.get('/me', getUserByToken);
 router.get('/:id', getUserById);
 router.patch('/me', editProfile);

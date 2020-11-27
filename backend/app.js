ca3cfa932590a97e7d4fea4b736236397bb2422a
Medjestik <ivan-kostyulin@yandex.ru>
2020-11-27 08:54:37 +0300

    fix: frontend

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const { validateUserData } = require('./middlewares/validation.js');
